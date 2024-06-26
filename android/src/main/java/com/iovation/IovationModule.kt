d72b5faa6ebf941934e897d5d5ef47ec6a386265
Felipe Cornejo <felicornejo@gmail.com>
2024-06-26 15:45:52 -0300

    feat: add jitpack dependency on android

--- android/src/main/java/com/iovation/IovationModule.kt
+++ android/src/main/java/com/iovation/IovationModule.kt
@@ -4,8 +4,8 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
-// import com.iovation.mobile.android.FraudForceConfiguration
-// import com.iovation.mobile.android.FraudForceManager
+import com.iovation.mobile.android.FraudForceConfiguration
+import com.iovation.mobile.android.FraudForceManager
 
 class IovationModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
@@ -19,22 +19,22 @@ class IovationModule(reactContext: ReactApplicationContext) :
     promise.resolve(a * b)
   }
 
-  // val configuration = FraudForceConfiguration.Builder()
-	 //  .subscriberKey("M1WrRSwcjUBQmHamij3DxQJWr00YzfRhXaMkI+zhhiY=")
-		// .enableNetworkCalls(true)
-		// .build()
+  val configuration = FraudForceConfiguration.Builder()
+	  .subscriberKey("M1WrRSwcjUBQmHamij3DxQJWr00YzfRhXaMkI+zhhiY=")
+		.enableNetworkCalls(true)
+		.build()
 
   val context = reactContext.getApplicationContext()
 
-	// init {
-	//   FraudForceManager.initialize(configuration, context)
- //    FraudForceManager.refresh(context)
-	// }
+	init {
+	  FraudForceManager.initialize(configuration, context)
+    FraudForceManager.refresh(context)
+	}
 
   @ReactMethod
-  fun getBlackbox(promise: Promise) {
-    // val blackbox = FraudForceManager.getBlackbox(context)
-    promise.resolve("test")
+  fun getBlackbox(a: Double, b:Double, promise: Promise) {
+    val blackbox = FraudForceManager.getBlackbox(context)
+    promise.resolve(blackbox)
   }
 
   companion object {
